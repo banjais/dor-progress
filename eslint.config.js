9598269864cea8e7841dfedda92b860a263dcc74
banjais <banjays@gmail.com>
2026-05-06 22:25:35 +0545

    Deploy

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginN from "eslint-plugin-n";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default [
-  {
-    ignores: ["dist", ".build", "node_modules", "*.config.js"],
-  },
-  js.configs.recommended,
-  {
-    files: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.ts"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-        ...globals.node,
-        ...globals.serviceworker,
-        console: "readonly",
-      },
-    },
-    plugins: {
-      n: pluginN,
-    },
-    rules: {
-      "no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-        },
-      ],
-      "no-console": "off",
-      "no-undef": "error",
-      "n/no-deprecated-api": "warn",
-    },
-  },
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
